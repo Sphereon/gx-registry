6ad8b6abeee676012080097e89b1ec209e8fdb6a
Moritz Kirstein <mkirstein@protonmail.com>
2022-03-10 18:29:47 +0100

    chore: remove unused code & add TODOs

--- src/routes/trustAnchor.route.ts
+++ src/routes/trustAnchor.route.ts
@@ -4,7 +4,6 @@ import validationMiddleware from '../middlewares/validation.middleware'
 import TrustAnchorController from '../controllers/trustAnchor.controller'
 import { Routes } from '../interfaces/routes.interface'
 import TrustAnchor from '../models/trustAnchor.model'
-import TrustAnchorList from '../models/trustAnchorList.model'
 import EiDASTrustedListParser from '../utils/parsers/EiDASTrustedListParser'
 import { logger } from '../utils/logger'
 
@@ -18,40 +17,12 @@ class TrustAnchorRoute implements Routes {
   }
 
   private initializeRoutes() {
-    //this.router.get(`${this.path}`, this.addToDb)
+    // TODO: remove GET route
     this.router.get(`${this.path}`, this.parseXml)
     this.router.post(`${this.path}`, validationMiddleware(RequestTrustAnchorDto, 'body'), this.trustAnchorController.getTrustAnchor)
   }
 
-  //TODO: remove after testing
-  private async addToDb(req: Request, res: Response) {
-    const trustAnchorList = TrustAnchorList
-    const trustAnchor = TrustAnchor
-
-    const list = await trustAnchorList.create({
-      name: 'Example Trusted List',
-      location: 'https://example.com',
-      type: 'CSV',
-      updateCycle: 604800000 // 1 week
-    })
-
-    const inserted = await trustAnchor.insertMany([
-      {
-        name: 'Anchor #1',
-        _list: list._id,
-        publicKey: '1234567890'
-      },
-      {
-        name: 'Trust Anchor #2',
-        _list: list._id,
-        publicKey: '0987654321'
-      }
-    ])
-
-    return res.status(200).json(inserted)
-  }
-
-  //TODO: remove after testing
+  // TODO: refactor into unit tests
   // ONLY used for testing. Currently fetches all lists on the lotl
   // and stores the TSPs into the DB as TrustAnchors
   private async parseXml(req: Request, res: Response) {
