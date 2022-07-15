1b582518dc6ec1f2fd499018049a7e21cc228f96
Moritz Kirstein <mkirstein@protonmail.com>
2022-07-15 11:35:06 +0000

    Fix: versioning

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { TrustAnchorModule } from './trust-anchor/trust-anchor.module'
 import { MongooseModule } from '@nestjs/mongoose'
-import { TrustAnchorModuleV1 } from './trust-anchor/trust-anchor-v1.module'
 import { TermsAndConditionsModule } from './terms-and-conditions/terms-and-conditions.module'
 import { ShapeModule } from './shape/shape.module'
 @Module({
@@ -22,7 +21,6 @@ import { ShapeModule } from './shape/shape.module'
       `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
     ),
     TrustAnchorModule,
-    TrustAnchorModuleV1,
     TermsAndConditionsModule,
     ShapeModule
   ],
