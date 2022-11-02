70ce7afe401acc5645134129cfcc870095942194
Alexander Eger <alex@delta-dao.com>
2022-11-02 15:50:33 +0100

    Update Swagger API Path

--- src/shape/constants.ts
+++ src/shape/constants.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import { join } from 'path'
 
-export const availableShapeFiles = fs.readdirSync(join(__dirname, '../static/shapes/v2206')).map(filename => filename.split('.')[0])
+export const availableShapeFiles = fs.readdirSync(join(__dirname, '../static/shapes/')).map(filename => filename.split('.')[0])
 
 export enum FileTypes {
   ttl,
